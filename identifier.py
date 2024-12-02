4a5837f4507594c5e659392960e6a4b50a6c3577
Shapyio <47731903+Shapyio@users.noreply.github.com>
2024-12-02 09:40:33 -0500

    Fixed issues from previous commit. Works. Added a way to save model training at end. Added script to test on individual images.

--- identifier.py
+++ identifier.py
@@ -1,18 +1,28 @@
 from ultralytics import YOLO
 import torch
+import os
 
-model = YOLO("yolov5nu.pt")
+if (os.path.exists("yolov5_trained.pt")):
+    model = YOLO("yolov5_trained.pt")
+else:
+    model = YOLO("yolov5nu.pt")
 
 model.info()
 
-model.train(
-    data="data.yaml",       # Path to the data.yaml file
-    epochs=50,              # Number of epochs
-    imgsz=640,              # Image size
-    batch=8,                # Batch size
-    val=True,               # Validate during training
-)
+ans = input("Do you want to train model? (y/n)")
+
+if ans == "y":
+    model.train(
+        data="data.yaml",       # Path to the data.yaml file
+        epochs=50,              # Number of epochs
+        imgsz=640,              # Image size
+        batch=8,                # Batch size
+        val=True,               # Validate during training
+    )
 
 results = model.val(data="data.yaml", split="test")
 
 print("RESULTS\n", results)
+
+# Save the trained model 
+model.save("yolov5_trained.pt")  # This explicitly saves the current weights
