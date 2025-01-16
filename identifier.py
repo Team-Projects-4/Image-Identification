6302f17f1fc81f5b4632706969d467922b1377f4
Shapyio <47731903+Shapyio@users.noreply.github.com>
2025-01-16 08:56:46 -0500

    Tested unlabeled images. Make sure you run script while in Image-Identification directory. Needs to be trained more, while it correctly predicts, it is not always confident.

--- identifier.py
+++ identifier.py
@@ -11,7 +11,7 @@ model.info()
 
 ans = input("Do you want to train model? (y/n) ")
 
-if ans == "y":
+if ans.lower() == "y":
     model.train(
         data="data.yaml",       # Path to the data.yaml file
         epochs=50,              # Number of epochs
@@ -20,9 +20,24 @@ if ans == "y":
         val=True,               # Validate during training
     )
 
-results = model.val(data="data.yaml", split="test")
+ans = input("Do you want to test individual image? (y/n) ")
+if ans.lower() == "y":
+    while True:
+        img_path = input("Enter the path to the image: ")   # Input file path
+        img_path = img_path.strip('"').strip("'")
+        if img_path == "exit":                              # If "exit" is inputted, break and exit
+            break
 
-print("RESULTS\n", results)
+        if os.path.exists(img_path):                        # Check if file path exists
+            results = model(img_path, 
+                            save=True, 
+                            save_crop=True, 
+                            save_txt=True, 
+                            conf=0.25)                      # Lowering confidence threshold so it makes predictions more often
+            print("RESULTS\n", results)  
+            print("Results saved for:", img_path)
+        else:
+            print("Image not found. Try a new path. Type 'exit' to exit.")
 
 # Save the trained model 
 model.save("yolov5_trained.pt")  # This explicitly saves the current weights
