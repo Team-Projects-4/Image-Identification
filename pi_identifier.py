d17b73a1d4ef3e262a5eca53326d01fbe35b1066
Liam Tolkkinen <liamtolkki@gmail.com>
2025-04-13 15:23:31 -0400

    changed output path

--- pi_identifier.py
+++ pi_identifier.py
@@ -56,7 +56,7 @@ def main():
     # Sort by confidence score and take top 10
     top_10 = [img for img, _ in sorted(death_star_predictions, key=lambda x: x[1], reverse=True)[:10]]
     # send the sorted images to the encryption directory 
-    destinationDir = os.path.expanduser("~/repos/Image-Encryption-Hashing/image_dir/")
+    destinationDir = os.path.expanduser("~/repos/Image_Identification/output")
     os.makedirs(destinationDir, exist_ok=True)
     # Print only file names
     for img in top_10:
